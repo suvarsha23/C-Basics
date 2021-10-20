1df4188c2a5ba495aaf559b81ef271cd004de680
suvarsha23 <92291438+suvarsha23@users.noreply.github.com>
2021-10-20 12:27:04 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,17 @@
+/* c Program to print age, cgpa, initial */
+#include<stdio.h>
+void main()
+{
+	//variable decleration
+	int age;
+	float cgpa;
+	char initial;
+	//initialization
+	age = 18;
+	cgpa = 8.9;
+	initial ='M';
+	//output
+	printf("age of the student is %d\n",age);
+	printf(" cgpa of the student is %lf\n",cgpa);
+	printf(" student surname is %c\n",initial);
+}
